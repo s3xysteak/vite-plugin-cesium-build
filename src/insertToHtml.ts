5271822b7c069cb6a64a25bfe239d5c8e109a576
s3xysteak <j.yulia@qq.com>
2023-11-27 23:32:56 +0800

    refactor: 方法统一接受options，选项类型不再可能为undefined

--- src/insertToHtml.ts
+++ src/insertToHtml.ts
@@ -1,12 +1,16 @@
 import type { Plugin } from 'vite'
+import type { BuildCesiumOptions } from './index'
 
-export const insertToHtml = (to: string): Plugin => ({
-  name: 'vite-plugin-cesium-build:insertToHtml',
-  apply: 'build',
-  transformIndexHtml: () => [
-    {
-      tag: 'script',
-      attrs: { src: `/${to}/Cesium.js` }
-    }
-  ]
-})
+export const insertToHtml = (options: BuildCesiumOptions): Plugin => {
+  const { to } = options
+  return {
+    name: 'vite-plugin-cesium-build:insertToHtml',
+    apply: 'build',
+    transformIndexHtml: () => [
+      {
+        tag: 'script',
+        attrs: { src: `/${to}/Cesium.js` }
+      }
+    ]
+  }
+}
--- src/setBaseUrl.ts
+++ src/setBaseUrl.ts
@@ -1,17 +1,21 @@
 import type { Plugin } from 'vite'
+import type { BuildCesiumOptions } from './index'
 
-export const setBaseUrl = (to: string): Plugin => ({
-  name: 'vite-plugin-cesium-build:setBaseUrl',
-  transform(code, id) {
-    if (!id.endsWith('/src/main.js') && !id.endsWith('/src/main.ts')) return
+export const setBaseUrl = (options: BuildCesiumOptions): Plugin => {
+  const { to } = options
+  return {
+    name: 'vite-plugin-cesium-build:setBaseUrl',
+    transform(code, id) {
+      if (!id.endsWith('/src/main.js') && !id.endsWith('/src/main.ts')) return
 
-    const val =
-      `
-      Object.defineProperty(globalThis, 'CESIUM_BASE_URL', {
-        value: '/${to}/'
-      });\n
-      ` + code
+      const val =
+        `
+        Object.defineProperty(globalThis, 'CESIUM_BASE_URL', {
+          value: '/${to}/'
+        });\n
+        ` + code
 
-    return val
+      return val
+    }
   }
-})
+}
