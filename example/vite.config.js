ec27d20ca9163b34608c44ac1bcd037a123d8f88
s3xysteak <j.yulia@qq.com>
2023-12-03 20:02:53 +0800

    chore: 更新example和版本号

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import { buildCesium } from 'vite-plugin-cesium-build'
+import cesium from 'vite-plugin-cesium-build'
 
 export default defineConfig({
-  plugins: [buildCesium()]
+  plugins: [cesium()]
 })
