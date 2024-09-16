c7fb4f61207037f034217aace96cce8a23db18e5
jerome.fayot <jerome.fayot@gmail.com>
2024-09-16 16:32:19 +0200

    fix: added css option flag

--- src/engine.ts
+++ src/engine.ts
@@ -5,36 +5,37 @@ import { type BuildCesiumOptions, copyCesium, importCss, resolveOptions, setBase
 function pluginEntry(pluginOptions?: Partial<BuildCesiumOptions>): Plugin[] {
   const options = resolveOptions(pluginOptions, 'node_modules/@cesium/engine')
 
+  const customCopyList = [
+    {
+      src: `${options.from}/Source/Assets/*`,
+      dest: `${options.to}/Assets/`,
+    },
+    {
+      src: `${options.from}/Source/ThirdParty/*.wasm`,
+      dest: `${options.to}/ThirdParty/`,
+    },
+    {
+      src: `${options.from}/Build/ThirdParty/*`,
+      dest: `${options.to}/ThirdParty/`,
+    },
+    {
+      src: `${options.from}/Build/Workers/*`,
+      dest: `${options.to}/Workers/`,
+    },
+  ]
+
+  if (options.css) {
+    customCopyList.push({
+      src: `${options.from}/Source/Widget/CesiumWidget.css`,
+      dest: `${options.to}/Widget/`,
+    })
+  }
+
   return [
     ...copyCesium(
       options,
       [],
-      [
-        {
-          src: `${options.from}/Source/Assets/*`,
-          dest: `${options.to}/Assets/`,
-        },
-        {
-          src: `${options.from}/Source/Widget/CesiumWidget.css`,
-          dest: `${options.to}/Widget/`,
-        },
-        {
-          src: `${options.from}/Source/ThirdParty/*.wasm`,
-          dest: `${options.to}/ThirdParty/`,
-        },
-        {
-          src: `${options.from}/Source/Widget/CesiumWidget.css`,
-          dest: `${options.to}/Widget/`,
-        },
-        {
-          src: `${options.from}/Build/ThirdParty/*`,
-          dest: `${options.to}/ThirdParty/`,
-        },
-        {
-          src: `${options.from}/Build/Workers/*`,
-          dest: `${options.to}/Workers/`,
-        },
-      ],
+      customCopyList,
     ),
 
     importCss(options, base => `${base}${options.from}/Source/Widget/CesiumWidget.css`, 'serve'),
